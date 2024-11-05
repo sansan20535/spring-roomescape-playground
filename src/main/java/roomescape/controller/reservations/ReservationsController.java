5fd52d77b9de7fba3c3570ab529a267b7689d9a3
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-11-05 19:27:46 +0900

    [FEAT] 2단계 - 예약 목록 조회 API 구현

--- src/main/java/roomescape/controller/reservations/ReservationsController.java
+++ src/main/java/roomescape/controller/reservations/ReservationsController.java
@@ -1,11 +1,39 @@
 package roomescape.controller.reservations;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.entity.ReservationEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationsController {
 
+    private List<ReservationEntity> reservationEntities = new ArrayList<>();
 
+    @GetMapping
+    public ResponseEntity<List<ReservationEntity>> getReservations() {
+        reservationEntities.add(
+                ReservationEntity.builder()
+                        .id(1)
+                        .name("브라운")
+                        .date("2023-01-01")
+                        .time("10:00")
+                        .build()
+        );
+        reservationEntities.add(
+                ReservationEntity.builder()
+                        .id(2)
+                        .name("브라운")
+                        .date("2023-01-02")
+                        .time("11:00")
+                        .build()
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(reservationEntities);
+    }
 }
