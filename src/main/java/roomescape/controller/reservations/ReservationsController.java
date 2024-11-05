dfba5e9ac1dd60bbedbd4041f69f1f4167708e1b
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-11-05 20:14:16 +0900

    [FEAT] 3단계 - 예약 추가 및 삭제 API 구현

--- src/main/java/roomescape/controller/reservations/ReservationsController.java
+++ src/main/java/roomescape/controller/reservations/ReservationsController.java
@@ -2,38 +2,62 @@ package roomescape.controller.reservations;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import roomescape.dto.request.ReservationRegisterRequest;
+import roomescape.dto.response.ReservationRegisterResponse;
 import roomescape.entity.ReservationEntity;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationsController {
 
     private List<ReservationEntity> reservationEntities = new ArrayList<>();
+    private AtomicLong index = new AtomicLong();
 
     @GetMapping
     public ResponseEntity<List<ReservationEntity>> getReservations() {
-        reservationEntities.add(
-                ReservationEntity.builder()
-                        .id(1)
-                        .name("브라운")
-                        .date("2023-01-01")
-                        .time("10:00")
-                        .build()
-        );
-        reservationEntities.add(
-                ReservationEntity.builder()
-                        .id(2)
-                        .name("브라운")
-                        .date("2023-01-02")
-                        .time("11:00")
-                        .build()
-        );
         return ResponseEntity.status(HttpStatus.OK).body(reservationEntities);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationRegisterResponse> createReservation(
+            @RequestBody final ReservationRegisterRequest reservationRegisterRequest
+    ) {
+        final ReservationEntity reservationEntity = ReservationEntity.builder()
+                .id(index.incrementAndGet())
+                .name(reservationRegisterRequest.name())
+                .date(reservationRegisterRequest.date())
+                .time(reservationRegisterRequest.time())
+                .build();
+
+        reservationEntities.add(reservationEntity);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("Location", URI.create("/reservations/" + reservationEntity.getId()).toString())
+                .body(ReservationRegisterResponse.of(
+                        reservationEntity.getId(),
+                        reservationEntity.getName(),
+                        reservationEntity.getDate(),
+                        reservationEntity.getTime()
+                ));
+    }
+
+    @DeleteMapping("/{reservationId}")
+    public ResponseEntity<Void> deleteReservation(
+            @PathVariable(name = "reservationId") final long reservationId
+    ) {
+        final ReservationEntity findReservationEntity = reservationEntities.stream()
+                .filter(reservationEntity -> reservationEntity.getId() == reservationId)
+                .findFirst()
+                .orElseThrow(null);
+
+        reservationEntities.remove(findReservationEntity);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 }
