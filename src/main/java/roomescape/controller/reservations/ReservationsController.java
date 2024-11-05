a4bd666c4b434cc5892a8df82b4f53732d772c0e
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-11-05 18:56:18 +0900

    [FEAT] 2단계 - 도메인 분리

---
+++ src/main/java/roomescape/controller/reservations/ReservationsController.java
@@ -0,0 +1,11 @@
+package roomescape.controller.reservations;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationsController {
+
+
+}
--- src/main/java/roomescape/controller/view/RoomescapeController.java
+++ src/main/java/roomescape/controller/view/RoomescapeController.java
@@ -1,4 +1,4 @@
-package roomescape.controller;
+package roomescape.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RoomescapeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String renderingHomePage() {
         return "home";
     }
+
+    @GetMapping("/reservation")
+    public String renderingReservationPage() {
+        return "reservation";
+    }
+
 }
