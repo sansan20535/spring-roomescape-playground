807fae2ebcf39c29b3dd937dd34492fb80b01c72
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-11-05 18:37:41 +0900

    [FEAT] 1단계 - thymeleaf 의존성 추가 및 테스트 확인

--- src/main/java/roomescape/controller/RoomescapeController.java
+++ src/main/java/roomescape/controller/RoomescapeController.java
@@ -1,8 +1,13 @@
 package roomescape.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoomescapeController {
 
+    @GetMapping("/")
+    public String renderingHomePage() {
+        return "home";
+    }
 }
