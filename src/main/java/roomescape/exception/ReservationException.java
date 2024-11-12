6e4fba63e188fe0bda3b018cd1a1bd4921944b91
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-11-12 18:51:31 +0900

    [REFACTOR] 접근 제어자 수정

--- src/main/java/roomescape/exception/ReservationException.java
+++ src/main/java/roomescape/exception/ReservationException.java
@@ -10,7 +10,7 @@ public class ReservationException extends RuntimeException {
     private final String message;
     private final HttpStatus httpStatus;
 
-    public ReservationException(final ErrorMessage errorMessage) {
+    protected ReservationException(final ErrorMessage errorMessage) {
         super(errorMessage.getMessage());
         this.message = errorMessage.getMessage();
         this.httpStatus = errorMessage.getHttpStatus();
